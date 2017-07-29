6acaa50aaa2eefe5c331a4ac97baed76bef0d05c
James H. Hill <hilljh82@gmail.com>
2017-07-29 01:26:37 -0400

    Added moment package to add-on

--- blueprints/ember-cli-chartjs/index.js
+++ blueprints/ember-cli-chartjs/index.js
@@ -4,8 +4,14 @@ module.exports = {
   normalizeEntityName() {}, // no-op since we're just adding dependencies
 
   afterInstall () {
-    return this.addBowerPackagesToProject([
-      {name: 'chart.js', target: '^2.6.0'}
-    ]);
+    return this.addAddonsToProject({
+      packages: [
+        {name: 'ember-cli-moment-shim', target: '3.3.3'}
+      ]
+    }).then (() => {
+      return this.addBowerPackagesToProject([
+        {name: 'chart.js', target: '^2.6.0'}
+      ]);
+    });
   }
 };
