61cab34ea059e4e8fbec418ee59b21ad48254e7f
James H. Hill <hilljh82@gmail.com>
2017-07-29 00:49:26 -0400

    Removed moment dependency

--- blueprints/ember-cli-chartjs/index.js
+++ blueprints/ember-cli-chartjs/index.js
@@ -4,15 +4,8 @@ module.exports = {
   normalizeEntityName() {}, // no-op since we're just adding dependencies
 
   afterInstall () {
-    return this.addAddonsToProject ({
-      packages: [
-        {name: 'ember-cli-moment-shim', target: '^3.3.3'}
-      ]
-    })
-    .then (() => {
-      return this.addBowerPackagesToProject([
-        {name: 'chartjs', target: '^2.6.0'}
-      ]);
-    });
+    return this.addBowerPackagesToProject([
+      {name: 'chart.js', target: '^2.6.0'}
+    ]);
   }
 };
