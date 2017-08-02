8b3c4a5482080762c9c1fb824aa967d0b44a47f3
James H. Hill <hilljh82@gmail.com>
2017-08-02 10:22:53 -0400

    Removed chartjs-plugin-zoom

--- index.js
+++ index.js
@@ -11,15 +11,5 @@ module.exports = {
       development: app.bowerDirectory + '/chart.js/dist/Chart.js',
       production:  app.bowerDirectory + '/chart.js/dist/Chart.min.js'
     });
-
-    app.import ({
-      development: app.bowerDirectory + '/hammerjs/hammer.js',
-      production:  app.bowerDirectory + '/hammerjs/hammer.min.js'
-    });
-
-    app.import ({
-      development: app.bowerDirectory + '/chartjs-plugin-zoom/chartjs-plugin-zoom.js',
-      production:  app.bowerDirectory + '/chartjs-plugin-zoom/chartjs-plugin-zoom.min.js'
-    });
   }
 };
